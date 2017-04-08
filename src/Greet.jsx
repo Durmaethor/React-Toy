4ecdc5553883406d593f76afce1781d6b017d00e
Ryan Kramer <rkramer613@gmail.com>
2017-04-08 11:07:41 -0600

    Styling the new H1

--- src/Greet.jsx
+++ src/Greet.jsx
@@ -1,5 +1,14 @@
 import React from 'react';
+import styled from 'styled-components';
 
 export default function Greet({ firstName, lastName}) {
-  return <h1>Hello {firstName} {lastName}. How are you?</h1>;
+  return (
+    <StyledGreet color='green'>
+        Hello {firstName} {lastName}. How are you?
+    </StyledGreet>
+  );
 }
+
+const StyledGreet = styled.h1`
+    color: ${props => props.color};
+`;
