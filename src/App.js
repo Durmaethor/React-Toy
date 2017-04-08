29673fdd02063d7f8faed87c837d7910af4418f4
Ryan Kramer <rkramer613@gmail.com>
2017-04-08 11:20:19 -0600

    Simple React App

--- src/App.js
+++ src/App.js
@@ -2,11 +2,29 @@ import React from 'react';
 import Greet from './Greet';
 
 
-function App({}) {
+function App({  }) {
+
+  const names=['Ryan', 'Thad', 'Gabe', 'Luis'];
+
   return (
     <div className='people'>
-      <Greet firstName='Ryan' lastName='Kramer' fontSize='20' />
-      <Greet firstName='Mikkel' lastName='Mentor' fontSize='50' />
+
+      {
+        names.map((name, i) => {
+          return (
+            <Greet
+            firstName = { name } 
+            lastName = 'Whatever'
+            fontSize = { (i +1) * 10 }
+            >
+            </Greet>
+          );
+        })
+      }
+
+      {/*<Greet firstName='Ryan' lastName='Kramer' fontSize='20' />
+      <Greet firstName='Mikkel' lastName='Mentor' fontSize='50' />*/}
+      
     </div>
   );
 }
