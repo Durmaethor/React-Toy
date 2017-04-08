2e4cca43ebef0ebd3bec1107b585fb7234b504ee
Ryan Kramer <rkramer613@gmail.com>
2017-04-08 11:11:56 -0600

    Added fontsize to the parent with a fontSize func

--- src/App.js
+++ src/App.js
@@ -5,8 +5,8 @@ import Greet from './Greet';
 function App({}) {
   return (
     <div className='people'>
-      <Greet firstName='Ryan' lastName='Kramer' />
-      <Greet firstName='Mikkel' lastName='Mentor' />
+      <Greet firstName='Ryan' lastName='Kramer' fontSize='20' />
+      <Greet firstName='Mikkel' lastName='Mentor' fontSize='50' />
     </div>
   );
 }
--- src/Greet.jsx
+++ src/Greet.jsx
@@ -1,9 +1,12 @@
 import React from 'react';
 import styled from 'styled-components';
 
-export default function Greet({ firstName, lastName}) {
+export default function Greet({ firstName, lastName, fontSize}) {
   return (
-    <StyledGreet color='green'>
+    <StyledGreet 
+        color='green'
+        fontSize={ fontSize }
+    >
         Hello {firstName} {lastName}. How are you?
     </StyledGreet>
   );
@@ -11,4 +14,5 @@ export default function Greet({ firstName, lastName}) {
 
 const StyledGreet = styled.h1`
     color: ${props => props.color};
+    font-size: ${props => props.fontSize + 'px'};
 `;
